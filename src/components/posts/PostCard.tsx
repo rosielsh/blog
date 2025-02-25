13768ed464965231764a6bcdda7d7e05b8f00cf5
suhwa <tnghk9611@naver.com>
2025-02-26 00:17:43 +0900

    feat: 게시물 카드에 아이콘 추가

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,5 +1,5 @@
 import { PostMetadata } from "@/lib/mdx";
-import { CalendarHeart } from "lucide-react";
+import { CalendarHeart, ChartColumnStacked } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -22,12 +22,20 @@ export function PostCard({ post }: PostCardProps) {
           </div>
         )}
         <div className="space-y-2 p-5">
-          <time className="text-sm flex align-center">
-            <CalendarHeart size={15} height={20} color="gray" />
-            <span className="ml-1">
-              {new Date(post.date).toLocaleDateString()}
-            </span>
-          </time>
+          <div className="flex justify-between items-center">
+            <time className="text-sm flex items-center">
+              <CalendarHeart size={15} height={20} color="gray" />
+              <span className="ml-1">
+                {new Date(post.date).toLocaleDateString()}
+              </span>
+            </time>
+
+            <div className="text-sm flex items-center">
+              <ChartColumnStacked size={15} height={20} color="gray" />
+              <span className="ml-1 text-green-100">{post.category}</span>
+            </div>
+          </div>
+
           <div className="h-28">
             <h2 className="text-xl font-bold my-2 text-overflow">
               {post.title}
