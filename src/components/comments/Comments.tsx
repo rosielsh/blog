ecca51a09002f51f96760e66de5ef29240bbe54f
suhwa <tnghk9611@naver.com>
2025-03-09 16:52:54 +0900

    fix: 2번 인코딩되는 문제를 해결하기 위해 디코딩 로직 추가

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -2,9 +2,19 @@
 
 import { useTheme } from "next-themes";
 import Giscus from "@giscus/react";
+import { useEffect, useState } from "react";
 
 export function Comments() {
   const { theme } = useTheme();
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    const decodedPath = decodeURIComponent(path);
+    setTerm(decodedPath);
+  }, []);
+
+  if (!term) return null;
 
   return (
     <section className="mt-10 pt-8 border-t">
@@ -14,7 +24,8 @@ export function Comments() {
         repoId="R_kgDONq_4Zw"
         category="Announcements"
         categoryId="DIC_kwDONq_4Z84CnPsE"
-        mapping="pathname"
+        mapping="specific"
+        term={term}
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="bottom"
